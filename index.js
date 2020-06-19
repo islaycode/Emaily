ab0d96cdf1ab1134be822edecc6f0b8df8f4bd72
nikzdev <nikzdev@gmail.com>
2020-06-19 16:43:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//Importing express
+const express = require('express')
+//Creating our app which uses express features
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send({ hi: "there" });
+});
+//Dynamic PORT binding for heroku
+const PORT = process.env.PORT || 5000;
+// || 5000 means port 5000 will be used in development enviorment by us, process.env will be port of heroku
+app.listen(PORT);

e4aa1c9e60bd37caa54ba6656f90c98dc3d1fe00
Nikhil <nikzdev@gmail.com>
2020-07-15 22:59:47 +0530

    changed prod file

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,5 @@
 // prod.js - production keys are here 
-module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: MONGO_URI,
-    cookieKey: COOKIE_KEY,
-}
+export const googleClientID = process.env.GOOGLE_CLIENT_ID;
+export const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
+export const mongoURI = MONGO_URI;
+export const cookieKey = COOKIE_KEY;

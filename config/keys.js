eed74080967de583b395780fd1e71e5e89e132f6
Nikhil <nikzdev@gmail.com>
2020-07-15 23:08:15 +0530

    modified keyjs

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 //keys.js figure out what set of credential to return
 
-const { model } = require("mongoose");
+const model  = require("mongoose");
 
 if(process.env.NODE_ENV == 'production'){
 // we are in production - return prod set of keys
--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,7 @@
 // prod.js - production keys are here 
-export const googleClientID = process.env.GOOGLE_CLIENT_ID;
-export const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
-export const mongoURI = MONGO_URI;
-export const cookieKey = COOKIE_KEY;
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: MONGO_URI,
+    cookieKey: COOKIE_KEY,
+};

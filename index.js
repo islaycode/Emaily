67ab8f3ff6af246acd048f429fb2b661cf9c71c8
Nikhil <nikzdev@gmail.com>
2020-07-21 16:24:24 +0530

    Added billing and client side app

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.use(passport.session()); // Starting session //Middleware
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === "production"){
+    // Express will serve up production assests like our main.js, or main.css file!
+    app.use(express.static('client/build'));
+
+    //Express will serve up the index.html if it doesn't recognize the route
+    const path = require('path');
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+
+}
+
 const PORT = process.env.PORT || 5000; //Dynamic PORT binding for heroku
 // || 5000 means port 5000 will be used in development enviorment by us, process.env will be port of heroku
 app.listen(PORT);
